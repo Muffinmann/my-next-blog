ccbdc405e79bcf8483015be3e1bbd57dfc8ea30b
conner <conner.zhang.gl@gmail.com>
2023-01-15 18:43:52 +0100

    setup

---
+++ lib/utils/cls.ts
@@ -0,0 +1,4 @@
+const cls = (...classNames: string[]) => {
+  return classNames.join(' ');
+};
+export default cls;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Head>
+        <title>my next blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
